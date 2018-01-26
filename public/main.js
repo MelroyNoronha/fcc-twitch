270e7e43e6f0330d910422dd75805de0ab71ba40
MelroyNoronha <melmax1208@gmail.com>
2018-01-26 23:54:34 +0530

    added dependency jsonp. fetch for userid functional in this commit

--- public/main.js
+++ public/main.js
@@ -1,7 +1,37 @@
-let twitchApiUrl = "https://wind-bow.gomix.me/twitch-api";
 
-fetchJsonp(twitchApiUrl)
-    .then(response => response.json())
-    .then((data) => {
-        console.log(data);
+let twitchApiUrl = "https://wind-bow.gomix.me/twitch-api/";
+
+const dataDiv = document.getElementById("dataDiv");
+
+let twitchStreamers = [
+    "dreamhackcs",
+    "skyzhar",
+    "freecodecamp",
+    "faceittv",
+    "terakilobyte",
+    "robotcaleb",
+    "sheevergaming",
+    "esl_sc2",
+    "ogamingsc2",
+    "jacksofamerica"
+];
+
+let twitchStreamerIds = [];
+
+function getUserIds() {
+    twitchStreamers.forEach((currentStreamer) => {
+        fetchJsonp(twitchApiUrl + "users/" + currentStreamer)
+            .then(response => response.json())
+            .then(data => twitchStreamerIds.push(data._id))
     })
+    return twitchStreamerIds;
+}
+getUserIds();
+
+//Task: use userId to get stream data || Status: currently unfinsihed
+function getStreamData() {
+    for (var currentId in twitchStreamerIds) {
+        console.log(currentId);
+    }
+}
+
